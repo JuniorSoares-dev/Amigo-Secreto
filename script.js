4e5d808027cf595ee9b801c940dfae0b4f2259c0
JuniorSoares-dev <olaviojunior67@gmail.com>
2026-02-10 22:28:07 -0300

    feat: adiciona validação para campo de nome vazio

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function adicionar(){
     let amigo = document.getElementById("nome-amigo");
     let lista = document.getElementById("lista-amigos");
+
+    if(amigo.value === ''){ 
+        alert ('Digite o Nome do Amigo');
+    return;
+}
+
+
 }
