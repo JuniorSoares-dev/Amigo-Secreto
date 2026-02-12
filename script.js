5f8ae28c671b89b9dd4d7443c8e964e0a92207d8
JuniorSoares-dev <olaviojunior67@gmail.com>
2026-02-12 00:11:25 -0300

    feat: adiciona nomes à lista de amigos

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function adicionar(){
+function adicionar() {
     let amigo = document.getElementById("nome-amigo");
     let lista = document.getElementById("lista-amigos");
 
-    if(amigo.value === ''){ 
-        alert ('Digite o Nome do Amigo');
-    return;
-}
-
+    if (amigo.value === '') {
+        alert('Digite o Nome do Amigo');
+        return;
+    }
+    if (lista.textContent === '') {
+        lista.textContent = amigo.value;
+    }
+    else {
+        lista.textContent += ', ' + amigo.value;
+    }
 
 }
