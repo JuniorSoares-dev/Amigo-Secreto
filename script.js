84f1d4543125d25a4cf1fb78c05189d2a8728a77
JuniorSoares-dev <olaviojunior67@gmail.com>
2026-02-14 21:09:12 -0300

    feat: implementa funcionalidade do botão Sortear com lógica circular

--- script.js
+++ script.js
@@ -25,6 +25,18 @@ function sortear() {
         alert('Adicione ao Menos 2 Amigos na Lista');
         return;
     }
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+    }
+
 }
 
 function embaralha(lista) {
